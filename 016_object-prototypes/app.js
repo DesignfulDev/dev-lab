e0131b5009dc7f09f52894ea6022a4acc33aca63
Mauricio Lacerda <lacerda@protonmail.com>
2019-08-04 12:28:29 -0300

    adding concepts to prototypal inheritance on section 016

--- 016_object-prototypes/app.js
+++ 016_object-prototypes/app.js
@@ -1,32 +1,32 @@
 'use strict';
 
 var user = {
-  firstName: 'Mauricio',
-  lastName: 'Lacerda',
-  age: 38,
-  birthday: new Date('1981-02-17T10:55:00.000-03:00'),
-  fullName: function() {
-    console.log(this.firstName + ' ' + this.lastName);
-    return 0;
-  }
+	firstName: 'Mauricio',
+	lastName: 'Lacerda',
+	age: 38,
+	birthday: new Date('1981-02-17T10:55:00.000-03:00'),
+	fullName: function() {
+		console.log(this.firstName + ' ' + this.lastName);
+		return 0;
+	}
 };
 
 var volkov = {
-  playerName: 'Mauricio',
-  charNAme: 'Lucan Volkov',
-  classLevels: [
-    {
-      className: 'wizard',
-      classLevel: 3
-    },
-    {
-      className: 'fighter',
-      classLevel: 1
-    }
-  ],
-  getClassLevel: function() {
-    return this.classLevels[0].classLevel + this.classLevels[1].classLevel;
-  }
+	playerName: 'Mauricio',
+	charNAme: 'Lucan Volkov',
+	classLevels: [
+		{
+			className: 'wizard',
+			classLevel: 3
+		},
+		{
+			className: 'fighter',
+			classLevel: 1
+		}
+	],
+	getClassLevel: function() {
+		return this.classLevels[0].classLevel + this.classLevels[1].classLevel;
+	}
 };
 
 let volkovLevel = volkov.getClassLevel();
@@ -38,11 +38,11 @@ console.log(user);
 
 // Every object that is defined with the object constructor 'new Object', or with the object literal {} inherits at least one prototype which is defined in the property '__proto__'
 
-// Question: if I use Object.create() and pass no arguments, will it inherit '__proto__'? Find out bellow.
-
 console.log(user.__proto__);
 
 // This means that any data or functionality that exists within an object's prototype can also be accessed by the object itself.
+
+// Question: if I use Object.create() and pass no arguments, will it inherit '__proto__'? Find out bellow.
 
 console.log(user.hasOwnProperty('firstName'));
 
@@ -52,16 +52,17 @@ console.log(user.hasOwnProperty('firstName'));
 
 // PROTOTYPE CHAINING
 
-// When a property or method is called from an object, JavaScript will first lookup within the own object definition for a property or method with that name. If it doesn't find it, it will lookup into the object prototype. It will continue to do so until it reaches the highest level prototype, which is the Object prototype
+// When a property is called from an object, JavaScript will first lookup within the object definition for a property matching the name. If it doesn't find it, it will then lookup into the object's prototype. It will continue to do so until it reaches the highest level prototype, which is the Object prototype
 
 user.hasOwnProperty = function() {
-  console.log("I'm a homonymous method");
+	console.log("I'm a homonymous method");
 };
 
 user.hasOwnProperty('firstName'); // since we defined a homonymous method, the call will invoke the method that was created in the object definition, and it won't reach its prototype method.
 
-console.log(user.__proto__.hasOwnProperty('firstName'));
-console.log(user);
+console.log(user.__proto__.hasOwnProperty('firstName')); // returns false, because the 'user.__proto__' does not have a property named 'firstName'.
+
+console.log(user); // only the created object has the 'firstName' property, but we can't check it with '.hasOwnProperty()' because 'user' now has a different method associated with that name.
 
 // An object prototype can have other prototypes which it inherits from. This is called 'prototype chaining'. The highest level prototype is the Object prototype, which all other objects inherit from.
 
@@ -71,19 +72,24 @@ console.log(arr.toString()); // 'toString' is defined in the Object prototype, t
 
 console.log('################################');
 
-// DEFINING PROTORYPES WITH Object.create
+// DEFINING PROTORYPES WITH Object.create()
+
+// When you want to create an object that has a specific prototype, you must use Object.create()
 
 // To create an object using Object.create(), first you need to create a prototype to pass into that method.
 
 const playerProto = {
-  charClass: 'wizard',
-  charLevel: 3
+	charClass: 'wizard',
+	charLevel: 3
 };
 
-const player = Object.create(playerProto); // When calling the method, you pass in the prototype you created.
-console.log(player);
+// You could of course pass the prototype directly into Object.create() without the need to assign it to a variable first, but this looks cleaner and more readable.
 
-// If you pass 'null' as an argument, the object created will have no prototypes, no properties, and no methods associated with it - not even the Object __proto__, which is the highest level prototype.
+const player = Object.create(playerProto); // When calling Object.create, you are required to pass in an object prototype as argument.
+
+console.log(player); // 'player' will inherit the properties from 'playerProto', and 'playerProto' will inherit the properties from the Object prototype. This is an example of prototype chaining
+
+// If you pass 'null' as an argument, the object created will have no prototypes, no properties, and no methods associated with it - not even the Object prototype, which is the highest level prototype.
 
 const nullObj = Object.create(null);
 console.log(nullObj);
--- 019_bracket-notation/app.js
+++ 019_bracket-notation/app.js
@@ -17,42 +17,4 @@ var user = {
 	}
 };
 
-// ACCESSING PROPERTIES WITH BRACKET NOTATION []
-
-// This can be done by using a string literal that matches the name of the property.
-
-console.log(user['lastName']); // logs 'Lacerda'
-
-// To invoke a method, we use the name of the method as the string literal and put the (parens) after the [brackets] to execute the function.
-
-user['fullName'](); // logs 'Mauricio Lacerda'
-
-console.log(user['greet']); // Otherwise we return the function itself, which can still be useful in some situations (ex: to pass it as an argument to another function).
-
-// WHY THIS IS POWERFUL?
-
-// Because now we can access properties and methods within an object using VARIABLES or EXPRESSIONS that evaluate to a string literal.
-
-console.log(user['birth' + 'day']); // logs 'user.birthday' property
-
-let propName; // as a variable, you can reuse it to access different properties and methods at different times
-
-propName = 'age';
-console.log(user[propName]); // logs 38
-
-propName = 'greet';
-user[propName](); // calls user.greet()
-
-// By using expressions to access properties in objects, we can create powerful constructs in our code (ex: we can use loops, if statements, functions, or any structure that evaluates to a string literal)
-
-let customer = {
-	address0: 'Ty Smith',
-	address1: '150 South Main',
-	address2: 'Somewhere, MT 00034'
-};
-
-let addr = '';
-
-for (let i = 0; i < 3; i++) {
-	console.log(customer['address' + i] + '\n'); // This will print the customer's full address nicely formatted to the console
-}
+// PROTOTYPAL INHERITANCE
