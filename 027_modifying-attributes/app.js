1684c93decdccd1547cecdc752b0422d34409866
Mauricio Lacerda <lacerda@protonmail.com>
2019-08-25 19:34:37 -0300

    modifying attributes on HTML elements

---
+++ 027_modifying-attributes/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// MODIFYING ATTRIBUTES ON HTML ELEMENTS
+
+/*
+
+.getAttribute('attribute');
+.setAttribute('attribute', 'value');
+.hasAttribute('attribute');
+.removeAttribute('attribute');
+
+*/
+
+let sq5 = document.querySelector('#sq05');
+console.log(sq5);
+
+let sq5att = sq5.getAttribute('class');
+console.log(sq5att);
+
+let hasId = sq5.hasAttribute('id');
+console.log(hasId);
+
+setTimeout(() => {
+	sq5.removeAttribute('class');
+}, 1500);
+
+setTimeout(() => {
+	sq5.setAttribute('class', 'square');
+}, 3000);
+
+setTimeout(() => {
+	sq5.setAttribute('class', 'square blueSquare');
+}, 4500);
